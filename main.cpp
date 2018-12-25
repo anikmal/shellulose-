81557fc95927260e9cbbd10a5b1b593d74c11c4e
Anik Mal <35171797+anikmal@users.noreply.github.com>
2018-12-25 10:44:08 +0530

    New Comments added for better understanding

--- main.cpp
+++ main.cpp
@@ -28,18 +28,18 @@ double ang,Rang,z,a,Iyy,d,Myy,V,dl,ll,l,Rang1,m,m1,m2,m3,m4,M,Hsum=0,Vsum=0;
     printf("moment of inertia %lf \n",Iyy);
     printf("enter the perferred angular intervals \n");                   //n= no of angular interval
     scanf("%d",&n);
-    Myy=(dl*2*ang*a+(ll*2*a*sin(ang)))*l*l*0.125;
+    Myy=(dl*2*ang*a+(ll*2*a*sin(ang)))*l*l*0.125;                         //Myy= longitudinal moment at mid span
     printf(" total load %lf \n",dl*ang*a+(ll*2*sin(ang)*a));
     printf("Myy=%lf \n",Myy);
-    V=(dl*2*ang*a+ll*2*sin(ang)*a)*l*0.5;
+    V=(dl*2*ang*a+ll*2*sin(ang)*a)*l*0.5;                                 //V=vertical shear force at transverse                       
     printf("shear %lf\n",V);
     for(i=0;i<=n;i++)
     {
         Rang1=Rang-(Rang*(i)/n);
         zdist[i]=a*(1-cos(Rang1*rad))-z;                                  //the angular interval.........Rang1=each interval angle
-        Q[i]=2*a*a*d*(sin(Rang1*rad)-(sin(ang)*Rang1/Rang));
-        Nx[i]=Myy*d*(a*(1-cos(Rang1*rad))-z)/Iyy;
-        Nxq[i]=0.5*V*Q[i]/Iyy;
+        Q[i]=2*a*a*d*(sin(Rang1*rad)-(sin(ang)*Rang1/Rang));              //Q= the first static moment upto any point under consideration
+        Nx[i]=Myy*d*(a*(1-cos(Rang1*rad))-z)/Iyy;                         //Nx=axial force in x-direction
+        Nxq[i]=0.5*V*Q[i]/Iyy;                                            //Nxq= shear force in x direction
 
         if(i==0 || i==n)
         {
@@ -53,7 +53,7 @@ double ang,Rang,z,a,Iyy,d,Myy,V,dl,ll,l,Rang1,m,m1,m2,m3,m4,M,Hsum=0,Vsum=0;
         dz[i]=ds[i]*sin(Rang1*rad);
         ss[i]=Nxq[i]*2/l;                          //ss[i]=specific shear
         ssh[i]=ss[i]*dy[i];                          //ssh[i]=horizontal force component of specific shear
-        ssv[i]=-ss[i]*dz[i];                          //ssh[i]=vertical force component of specific shear
+        ssv[i]=-ss[i]*dz[i];                          //ssv[i]=vertical force component of specific shear
         dw[i]=dl*ds[i];                              //dw=dead weight
         lw[i]=ll*dy[i];                              //lw= live weight
         v[i]=ssv[i]+dw[i]+lw[i];
@@ -62,6 +62,7 @@ for(i=0;i<=n;i++)
 {
         printf("POINT%d Nx=%lf  Nxq=%lf\n",i,Nx[i],Nxq[i]);
 }
+// Transverse Moment Mθ
 m=Rang/n;    //m= angular interval
 for(i=0;i<n;i++)
 {
@@ -74,7 +75,7 @@ for(i=0;i<n;i++)
        M=m1+m2+m3+m4+M;
 
    }
-  Moment[i+1]=M;          //determinate moment
+  Moment[i+1]=M;          //determinate transverse moment Mθ
    M=0;
 }
 Moment[0]=0;
@@ -83,7 +84,7 @@ for(i=0;i<=n;i++)
 
     printf("transverse moment%d %lf\n",i,Moment[i]);
 }
-//Nq calculation
+//Axial force in θ direction i.e Nθ calculation
 for(i=0;i<=n;i++)
 {
     Rang1=Rang-(Rang*(i)/n);
